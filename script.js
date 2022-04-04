56d29e6e4937b7ed48c48900e70fac20649ecaa8
Yogii <stevenp0294@gmail.com>
2022-04-04 17:50:43 -0400

    Made an operand function to apply a chosen     operator to two selected numbers.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//simple math operator functions.
 function add(x, y) {
     return x + y;
 };
@@ -13,8 +14,20 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 };
-//simple math operator functions.
 
+//function to take two numbers and apply an operand to them.
 function operate(x, y, operand) {
-
+    switch(operand){
+        case '+': return add(x, y)
+            break;
+        case '-': return subtract(x, y)
+            break;
+        case 'x': return multiply(x, y)
+            break;
+        case '/': return divide(x, y)
+            break;
+    }
 }
+
+console.log(operate(2, 18, 'x'));
+//
