078b747042a74d5f6532a56999446071fa53bc49
Yogii <stevenp0294@gmail.com>
2022-04-07 14:08:39 -0400

    Finished CSS. Began attaching eventListeners
    Made some functions to store number variables to run though my operate
    function.
    
    Still needs to be finalized.
    displayValue is not becoming what it should be.
    calculator just doubles the first number pressed, no matter what.

--- script.js
+++ script.js
@@ -15,35 +15,128 @@ const divide = document.getElementById('divide');
 const equals = document.getElementById('equals');
 const decimal = document.getElementById('decimal');
 const clear = document.getElementById('clear');
+const workingTree = document.getElementById('working-tree');
+const result_div = document.getElementById('result');
+
+let displayValue = 0;
+let result = 0;
+
+let num1 = 0;
 
 //simple math operator functions.
-function add(x, y) {
+function addition(x, y) {
     return x + y;
 };
 
-function subtract(x, y) {
+function subtraction(x, y) {
     return x - y;
 };
 
-function multiply(x, y) {
+function multiplication(x, y) {
     return x * y;
 };
 
-function divide(x, y) {
+function division(x, y) {
     return x / y;
 };
 
 //function to take two numbers and apply an operand to them.
 function operate(x, y, operand) {
     switch(operand){
-        case '+' : return add(x, y)
+        case '+' : return addition(x, y)
           break;
-        case '-': return subtract(x, y)
+        case '-': return subtraction(x, y)
           break;
-        case 'x': return multiply(x, y)
+        case 'x': return multiplication(x, y)
           break;
-        case '/': return divide(x, y)
+        case '/': return division(x, y)
           break;
     }
 }
 
+// function to reset all values to zero or empty for
+// the next equation.
+function clearAll() {
+  displayValue = 0;
+  workingTree.textContent = '';
+  result = 0;
+  result_div.textContent = 0;
+}
+
+// function to begin equation. Assigns numbers to the display.
+function startEquation(num) {
+  workingTree.textContent += num;
+  displayValue = parseInt(workingTree.textContent);
+  console.log(workingTree.textContent);
+};
+
+
+
+//Start of eventListeners//
+one.addEventListener('click', () => {
+  startEquation(1);
+});
+
+two.addEventListener('click', () => {
+  startEquation(2);
+});
+
+three.addEventListener('click', () => {
+  startEquation(3);
+});
+
+four.addEventListener('click', () => {
+  startEquation(4);
+});
+
+five.addEventListener('click', () => {
+  startEquation(5);
+});
+
+six.addEventListener('click', () => {
+  startEquation(6);
+});
+
+seven.addEventListener('click', () => {
+  startEquation(7);
+});
+
+eight.addEventListener('click', () => {
+  startEquation(8);
+});
+
+nine.addEventListener('click', () => {
+  startEquation(9);
+});
+
+zero.addEventListener('click', () => {
+  startEquation(0);
+});
+
+equals.addEventListener('click', () => {
+  operate();
+});
+
+clear.addEventListener('click', () => {
+  clearAll();
+});
+
+add.addEventListener('click', () => {
+  num1 += displayValue;
+  displayValue = 0;
+  console.log(num1);
+  workingTree.textContent = num1 + ' + ';
+});
+
+subtract.addEventListener('click', () => {
+
+});
+
+multiply.addEventListener('click', () => {
+
+});
+
+divide.addEventListener('click', () => {
+
+});
+//End of eventListeners//
